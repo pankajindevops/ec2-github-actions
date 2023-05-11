0482ee491b376839f922ef7fbe1674b51f60852f
pankaj <pankajindevops@gmail.com>
2023-05-11 11:08:02 +0100

    Committed all files

---
+++ main.tf
@@ -0,0 +1,37 @@
+# Download the provider files for AWS
+terraform {
+  # Run init/plan/apply with "backend" commented-out (ueses local backend) to provision Resources (Bucket, Table)
+  # Then uncomment "backend" and run init, apply after Resources have been created (uses AWS)
+  backend "s3" {
+    bucket         = "pankaj-0710-state-backend"
+    key            = "tf-infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.30.0"
+    }
+  }
+}
+
+provider "aws" {
+
+  region = var.aws_region
+}
+
+/*****
+# Dynamo DB Table for Locking TF Config
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+*****/
---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "aws_region" {
+  description = "Name of the Region to be created"
+  default     = "us-east-1"
+}
+
+variable "instance_name" {
+  description = "Name of the instance to be created"
+  default     = "EC2 Instance for test purposes"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "vpc_id" {
+  description = "The VPC subnet the instance(s) will be created in"
+  default     = "vpc-780eab02"
+}
+
+variable "ami_id" {
+  description = "The AMI to use"
+  default     = "ami-007855ac798b5175e"
+}
+
+variable "number_of_instances" {
+  description = "number of instances to be created"
+  default     = 1
+}
+
+variable "ami_key_pair_name" {
+  default = "23-april-2023-key-pair"
+}
